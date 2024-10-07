960d07d2d09521d401faf17cac7dcffc17c2bdbe
devg1120 <devg1120@gmail.com>
2024-10-07 23:10:15 +0900

    up

---
+++ docsy_gusa_themes_demo3/assets/js/toggle.js
@@ -0,0 +1,19 @@
+/*!
+ * This is a Docsy-adapted version of https://github.com/twbs/examples/blob/main/color-modes/js/color-modes.js.
+ *
+ * Original header:
+ *
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2024 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+
+(() => {
+  'use strict'
+
+
+  const updateToggle = () => {
+               console.log("updateToggle")
+  }
+
+})()
